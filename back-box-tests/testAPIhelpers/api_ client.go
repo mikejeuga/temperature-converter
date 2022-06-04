48e5e212f3f1fb1d9034dc487232f261fb564a7b
Mike Jeuga <mikejeuga@gmail.com>
2022-06-04 17:15:31 +0100

    Shuffle things around

--- back-box-tests/testAPIhelpers/api_ client.go
+++ back-box-tests/testAPIhelpers/api_ client.go
@@ -14,30 +14,13 @@ type APIClient struct {
 	httpDriver *http.Client
 }
 
-func (c *APIClient) ConvertFtoC(temp models.Fahrenheit) (models.Celsius, error) {
-	url := c.baseURL + "/to-celsius/" + fmt.Sprintf("%v", temp)
-
-	resp, err := c.httpDriver.Get(url)
-	if err != nil {
-		return 0, fmt.Errorf("problem reaching out %s, %w", url, err)
+func NewAPIClient() *APIClient {
+	baseURL := "http://localhost:8069"
+	client := &http.Client{
+		Transport: http.DefaultTransport,
+		Timeout:   5 * time.Second,
 	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return 0, fmt.Errorf("unexpected response code %d but expected %d from %s", resp.StatusCode, http.StatusOK, url)
-	}
-
-	fahrenheit, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return 0, err
-	}
-
-	temperatureResponse, err := strconv.ParseFloat(string(fahrenheit), 64)
-	if err != nil {
-		return 0, err
-	}
-
-	return models.Celsius(temperatureResponse), nil
+	return &APIClient{baseURL: baseURL, httpDriver: client}
 }
 
 func (c *APIClient) ConvertCtoF(temp models.Celsius) (models.Fahrenheit, error) {
@@ -66,11 +49,28 @@ func (c *APIClient) ConvertCtoF(temp models.Celsius) (models.Fahrenheit, error) 
 	return models.Fahrenheit(temperatureResponse), nil
 }
 
-func NewAPIClient() *APIClient {
-	baseURL := "http://localhost:8069"
-	client := &http.Client{
-		Transport: http.DefaultTransport,
-		Timeout:   5 * time.Second,
+func (c *APIClient) ConvertFtoC(temp models.Fahrenheit) (models.Celsius, error) {
+	url := c.baseURL + "/to-celsius/" + fmt.Sprintf("%v", temp)
+
+	resp, err := c.httpDriver.Get(url)
+	if err != nil {
+		return 0, fmt.Errorf("problem reaching out %s, %w", url, err)
 	}
-	return &APIClient{baseURL: baseURL, httpDriver: client}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return 0, fmt.Errorf("unexpected response code %d but expected %d from %s", resp.StatusCode, http.StatusOK, url)
+	}
+
+	fahrenheit, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return 0, err
+	}
+
+	temperatureResponse, err := strconv.ParseFloat(string(fahrenheit), 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return models.Celsius(temperatureResponse), nil
 }
